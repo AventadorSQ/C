beb8d772ab9a5840157cf9a6108b501ab2dc93ef
AventadorSQ <2630420233@qq.com>
2018-11-14 17:47:42 +0800

    C++类定义

--- C++入门试炼4/C++入门试炼4/C++试炼.cpp
+++ C++入门试炼4/C++入门试炼4/C++试炼.cpp
@@ -4,18 +4,18 @@
 
 using namespace std;
 
- class Student 
-{
-	void SetStudentInfo(char* name, char* gender, int age)
-	{
-		strcpy(_name, name);
-		strcpy(_gender, gender);
-		_age = age;
-	}
-	char _name[1024];
-	char _gender[1024];
-	int _age;
-}Stu;
+// class Student 
+//{
+//	void SetStudentInfo(char* name, char* gender, int age)
+//	{
+//		strcpy(_name, name);
+//		strcpy(_gender, gender);
+//		_age = age;
+//	}
+//	char _name[1024];
+//	char _gender[1024];
+//	int _age;
+//}Stu;
 
  //类的定义
  //1.0类的声明与定义全部放在类中
