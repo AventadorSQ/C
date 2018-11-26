a9ec48c1d5cdc9243505f387ef069279253054f3
AventadorSQ <2630420233@qq.com>
2018-11-26 23:23:58 +0800

    顺序表头插

---
+++ Project64/Project64/SeqList.h
@@ -0,0 +1,20 @@
+#pragma once
+
+
+
+//Í·É¾
+//void SeqListPopFront(SeqList* s1)
+//{
+//	assert(s1);
+//	if (s1->size == 0)
+//	{
+//		printf("¿Õ¡£\n");
+//		return;
+//	}
+//
+//	for (int i = 1; i < s1->size; i++)
+//	{
+//		s1->data[i - 1] = s1->data[i];	
+//	}
+//	s1->size--;
+//} 
---
+++ Project64/Project64/database.c
@@ -0,0 +1,22 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include "SeqList.h"
+#include<stdlib.h>
+
+#define DataType int
+
+typedef struct SeqList
+{
+	DataType data;
+	int size;
+	int rongliang;
+};
+
+int main()
+{
+
+	system("pause");
+	return 0;
+}
